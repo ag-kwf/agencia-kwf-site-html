b5e4f882caf6c174fe87258dd25ed3e6c69d1114
ag-kwf <ag-kwf@users.noreply.github.com>
2026-04-09 22:09:11 +0000

    deploy: 86d4bc5956df8f8db9314c247fec090f1df50a51

--- deploy-webhook.php
+++ deploy-webhook.php
@@ -12,17 +12,20 @@ if (!hash_equals($hash, $signature)) {
     die('Unauthorized');
 }
 
-// Path to the git repository
+// Use cPanel UAPI to update and deploy the repository
+$cpanelUser = 'saiufa76';
 $repoPath = '/home2/saiufa76/repositories/agencia-kwf-site-html';
-$deployPath = '/home2/saiufa76/agenciakwf.com.br';
 
-// Pull latest changes and copy files
+// Call cPanel API via local socket
+$updateCmd = "/usr/local/cpanel/bin/uapi --user=$cpanelUser VersionControl update repo_path=$repoPath branch=deploy 2>&1";
+$deployCmd = "/usr/local/cpanel/bin/uapi --user=$cpanelUser VersionControl deploy repo_path=$repoPath 2>&1";
+
 $output = [];
-exec("cd $repoPath && /usr/local/cpanel/3rdparty/bin/git pull origin deploy 2>&1", $output);
-exec("cp -R $repoPath/* $deployPath/ 2>&1", $output);
+exec($updateCmd, $output);
+exec($deployCmd, $output);
 
 // Log the result
-$log = date('Y-m-d H:i:s') . " - Deploy executed\n" . implode("\n", $output) . "\n\n";
+$log = date('Y-m-d H:i:s') . " - Deploy via UAPI\n" . implode("\n", $output) . "\n\n";
 file_put_contents('/home2/saiufa76/deploy.log', $log, FILE_APPEND);
 
 http_response_code(200);
